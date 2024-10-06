4afdfb88cc798e06be87ecfb6777ce8654146db3
chetan.handa@gmail.com <chetan.handa@gmail.com>
2024-10-06 19:20:55 +0000

    Additional comments

--- 1-Feature store/2-use-feature-store.py
+++ 1-Feature store/2-use-feature-store.py
@@ -18,27 +18,35 @@ display(df)
 
 # COMMAND ----------
 
-inference_df = df.select("wine_id", "quality", (10 * rand()).alias("real_time_measurement")).orderBy("real_time_measurement")
-display(inference_df)
+# contains some features having newer values which will not be present in the feature store
+# wine_id will be the primary key based on which this raw dataset will be joined to the dataframe
+raw_new_df = df.select("wine_id", "quality", (10 * rand()).alias("real_time_measurement")).orderBy("real_time_measurement")
+display(raw_new_df)
 
 # COMMAND ----------
 
 fs = feature_store.FeatureStoreClient()
 
 def load_data(table_name, lookup_key):
+    
+    #get the features and the cooresponding lookup key based on which the raw dataset will be joined to the feature store
     model_feature_lookups = [FeatureLookup(table_name, lookup_key)]
 
-    training_set = fs.create_training_set(inference_df,model_feature_lookups,label="quality", exclude_columns="wine_id")
-    training_pd = training_set.load_df().toPandas()
+    #training set = aggregate of the features from the dataframe and the feature store 
+    training_set = fs.create_training_set(
+        df=raw_new_df, # this is the raw data used for training, can contain some newer data which may not be present in the feature store
+        feature_lookups=model_feature_lookups, # all the features to include in the training set, thes will be joined into the inference_df
+        label="quality", # target column for supervised learning, can be set to "none" for unsupervised learning 
+        exclude_columns="wine_id" # exclude the features which are not going to add any value
+        )
+    
+    training_pd = training_set.load_df().toPandas() # convert the aggregate dataframe to Pandaas dataframe 
 
     X = training_pd.drop(["quality"], axis=1)
-    y= training_pd["quality"]
-    X_train,X_test,y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+    y = training_pd["quality"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    return X_train,X_test,y_train,y_test,training_set
-    
-
-
+    return X_train, X_test, y_train, y_test, training_set
 
 # COMMAND ----------
 
@@ -67,16 +75,17 @@ except:
 
 # COMMAND ----------
 
+
 mlflow.sklearn.autolog(log_models=False)
 
 def train_model(X_train,y_train, X_test,y_test,training_set,fs):
     with mlflow.start_run() as run:
-        rf = RandomForestRegressor(max_depth=3,n_estimators=20, random_state=42)
-        rf.fit(X_train,y_train)
-        y_pred = rf.predict(X_test)
+        rf = RandomForestRegressor(max_depth=3,n_estimators=20, random_state=42) # set the hyperparameters
+        rf.fit(X_train,y_train) # try different hyperparameters and features
+        y_pred = rf.predict(X_test)  # compare the predicted values with the actual values in X_test
 
-        mlflow.log_metric("test_mse",mean_squared_error(y_test,y_pred))
-        mlflow.log_metric("test_r2_score",r2_score(y_test,y_pred))
+        mlflow.log_metric("test_mse",mean_squared_error(y_test,y_pred)) # log the mean square error
+        mlflow.log_metric("test_r2_score",r2_score(y_test,y_pred)) # log the mean r2 score
 
         fs.log_model(
             model = rf,
@@ -88,4 +97,8 @@ def train_model(X_train,y_train, X_test,y_test,training_set,fs):
 
 
 train_model(X_train,y_train,X_test,y_test,training_set,fs)                          
+
+
+# COMMAND ----------
+
 
