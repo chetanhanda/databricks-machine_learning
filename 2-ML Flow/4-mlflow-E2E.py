2aa44cfd4b58da29320023181ffd4c145992bd4d
chetan.handa@gmail.com <chetan.handa@gmail.com>
2024-10-10 01:47:59 +0000

    E2E with MLFlow

--- 2-ML Flow/4-mlflow-E2E.py
+++ 2-ML Flow/4-mlflow-E2E.py
@@ -8,8 +8,30 @@ display(df)
 
 # COMMAND ----------
 
-white_wine = pd.read_csv("/dbfs/mnt/something/winequality-white.csv",sep=";")
-red_wine = pd.read_csv("/dbfs/mnt/something/winequality-red.csv",sep=";")
+white_wine =  spark.read.csv("/mnt/something/winequality-white.csv",sep=";", header=True)
+red_wine = spark.read.csv("/mnt/something/winequality-red.csv",sep=";", header=True)
+
+display(white_wine)
+
+# COMMAND ----------
+
+def format_column_names(df):
+    """Rename columns """
+    renamed_df = df
+    for col in df.columns:                
+        renamed_df = renamed_df.withColumnRenamed(col, col.replace(" ", "_").lower())                                          
+    return renamed_df
+
+# COMMAND ----------
+
+# concat the two datasets
+concat_df = white_wine.union(red_wine)
+display(concat_df)
+
+# COMMAND ----------
+
+renamed_df = format_column_names(concat_df) 
+display(renamed_df) 
 
 # COMMAND ----------
 
