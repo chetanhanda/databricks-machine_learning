16f17343ebaa5ebdbec04f68984f24546d503cc6
chetan.handa@gmail.com <chetan.handa@gmail.com>
2024-10-10 03:45:21 +0000

    Using pyspark panda instead of spark dataframe

--- 2-ML Flow/4-mlflow-E2E.py
+++ 2-ML Flow/4-mlflow-E2E.py
@@ -1,5 +1,7 @@
 # Databricks notebook source
-import pandas as pd
+#import pandas as pd
+import pyspark.pandas as pd
+
 
 # COMMAND ----------
 
@@ -8,10 +10,15 @@ display(df)
 
 # COMMAND ----------
 
-white_wine =  spark.read.csv("/mnt/something/winequality-white.csv",sep=";", header=True)
-red_wine = spark.read.csv("/mnt/something/winequality-red.csv",sep=";", header=True)
+white_wine =  pd.read_csv("/mnt/something/winequality-white.csv",sep=";")
+red_wine = pd.read_csv("/mnt/something/winequality-red.csv",sep=";")
 
+red_wine['is_red']=1
+white_wine['is_red']=0
+
+display(red_wine)
 display(white_wine)
+
 
 # COMMAND ----------
 
@@ -25,13 +32,34 @@ def format_column_names(df):
 # COMMAND ----------
 
 # concat the two datasets
-concat_df = white_wine.union(red_wine)
+concat_df = pd.concat([white_wine, red_wine], ignore_index=True)
 display(concat_df)
 
 # COMMAND ----------
 
-renamed_df = format_column_names(concat_df) 
-display(renamed_df) 
+# renamed_df = format_column_names(concat_df) 
+renamed_df = concat_df.rename(columns=lambda x: x.replace(" ", "_").lower())
+display(renamed_df)
+
+# COMMAND ----------
+
+display(renamed_df.describe)
+
+# COMMAND ----------
+
+renamed_df.corr
+
+# COMMAND ----------
+
+display(renamed_df.head())
+
+# COMMAND ----------
+
+renamed_df.dtypes
+
+# COMMAND ----------
+
+renamed_df.describe()
 
 # COMMAND ----------
 
